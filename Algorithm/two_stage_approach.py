305360d8d8a1d8cf381d8eb6f85a326481006a9d
zhong-gz <510475503@qq.com>
2024-12-26 15:26:13 +0800

    update

--- Algorithm/two_stage_approach.py
+++ Algorithm/two_stage_approach.py
@@ -5,7 +5,6 @@ class two_stage_algo:
     def __init__(self,X_base,y_base,tol = 1e-6):
         self.X_base = np.copy(X_base)
         self.y_base = np.copy(y_base)
-        self.y_base[self.y_base != 1] = 0
         self.theta = None
         self.tol = tol
         self.mu_x = None
@@ -18,8 +17,9 @@ class two_stage_algo:
         n = len(self.y_base)
         repeat_theta = self.theta_list
 
-        theta_theta_inv = np.linalg.inv(repeat_theta.T @ repeat_theta)
-        theta_theta_inv = theta_theta_inv + 0.0001*np.eye(theta_theta_inv.shape[0])
+        theta_t_theta = repeat_theta.T @ repeat_theta
+
+        theta_theta_inv = np.linalg.inv(theta_t_theta + 0.0001*np.eye(theta_t_theta.shape[0]))
 
         self.mu_x = theta_theta_inv @ repeat_theta.T @ self.X_shift
         self.mu_y = theta_theta_inv @ repeat_theta.T @ self.y_shift 
@@ -41,7 +41,7 @@ class two_stage_algo:
         theta_init = np.random.randn(d)
         learning_rate = 0.01
         num_iterations = 10000
-        theta_final, cost_history = self.gradient_descent(self.X_base, y, self.mu_x,self.mu_y, theta_init, learning_rate, num_iterations)
+        theta_final, cost_history = self.gradient_descent(self.X_base, self.y_base, theta_init, learning_rate, num_iterations)
 
         self.theta = np.copy(theta_final)
         self.theta_list = np.concatenate((self.theta_list, np.tile(theta_final,(n, 1))), axis=0)
@@ -54,22 +54,20 @@ class two_stage_algo:
 
     def compute_cost(self,X, y, theta):
         m = len(y)
-        theta_mu_y = np.repeat(theta @ self.mu_y, m, axis=0)
-        theta_mu_x = np.repeat(theta @ self.mu_x, m, axis=0)
-        cost = np.linalg(y+theta_mu_y - (X + theta_mu_x).T @ theta)
+        theta_mu_y = np.repeat(theta @ self.mu_y, m, axis=0).reshape(-1, 1)
+        theta_mu_x = np.tile(theta @ self.mu_x, (m, 1))
+        cost = np.linalg.norm(y+theta_mu_y - ((X + theta_mu_x) @ theta).reshape(-1, 1))
         return cost
 
-    def gradient_descent(self,X, y,mu , theta, learning_rate, num_iterations):
+    def gradient_descent(self,X, y,theta, learning_rate, num_iterations):
         m = len(y)
         cost_history = []
-        mu = np.insert(mu, 0, 0)
-
-        for i in range(num_iterations):
+        for j in range(num_iterations):
             gradient = np.zeros_like(theta)
             for i in range(m):
-                gradient = gradient + y[i]@self.mu_y + theta @self.mu_y@self.mu_y - X[i,:].T @ theta @ self.mu_y + self.mu_x.T@theta.T@theta@self.mu_y\
-                                    - y[i] @ X[i,:].T - theta@self.mu_y@X[i,:].T + X[i,:].T @ theta @X[i,:].T - self.mu_x.T@theta.T@theta@X[i,:].T\
-                                    + y[i] @self.mu_x.T@theta.T + theta@self.mu_y@self.mu_x.T@theta.T - X[i,:].T @ theta @self.mu_x.T@theta.T + self.mu_x.T@theta.T@theta@self.mu_x.T@theta.T
+                gradient = gradient + (y[i]*self.mu_y + theta @self.mu_y * self.mu_y - X[i,:].T @ theta * self.mu_y + self.mu_x.T@theta.T@theta*self.mu_y).reshape(-1)\
+                                    - y[i] * X[i,:].T - theta@self.mu_y*X[i,:].T + X[i,:].T @ theta *X[i,:].T - self.mu_x.T@theta.T@theta*X[i,:].T\
+                                    + y[i] *self.mu_x.T@theta.T + theta@self.mu_y*self.mu_x.T@theta.T - X[i,:].T @ theta *self.mu_x.T@theta.T + self.mu_x.T@theta.T@theta*self.mu_x.T@theta.T
             gradient = 2 * gradient
             theta -= learning_rate * gradient
             cost = self.compute_cost(X, y, theta)
