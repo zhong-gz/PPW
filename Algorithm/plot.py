d376e4be0efa359eca7a5ae06550b496bb3143b0
ZHONG-GZ <510475503@qq.com>
2025-01-07 16:38:42 +0800

    refactor: update plotting function and add new result CSV files for model evaluation

--- Algorithm/plot.py
+++ Algorithm/plot.py
@@ -22,10 +22,10 @@ def format_number(number):
 def plot_fig(num_iters = 25,d_list = [10,1000,10000],folder_path = 'result/'):
     num_d  = len(d_list)
     data_dict = {}
-    # methods = ['PPW-AVG','PPW-EMA','RRM_Linear_Regression','RGD_Linear_Regression','RRM with Neural Networks','Two-Stage Approach','PerGD','DFO']
+    methods = ['PPW-AVG','PPW-EMA','RRM_Linear_Regression','RGD_Linear_Regression','RRM with Neural Networks','Two-Stage Approach','PerGD','DFO']
     # methods = ['PPW-AVG','PPW-EMA','RRM_Linear_Regression','RGD_Linear_Regression','PerGD']
     # methods = ['RGD_Linear_Regression']
-    methods = ['PPW-AVG','PPW-EMA','RRM_Linear_Regression','RGD_Linear_Regression','Two-Stage Approach','PerGD','DFO'] #
+    # methods = ['PPW-AVG','PPW-EMA','RRM_Linear_Regression','RGD_Linear_Regression','Two-Stage Approach','PerGD','DFO'] #
 
     for methods_name in methods:
         data = np.load(folder_path+methods_name + '.npz')
@@ -161,8 +161,8 @@ def plot_fig(num_iters = 25,d_list = [10,1000,10000],folder_path = 'result/'):
         model_gaps_avg = inner_dict.get('model_gaps_avg')
         means = np.mean(model_gaps_avg[:,5:], axis=1)
         stds = np.std(model_gaps_avg[:,5:], axis=1)
-        # data = [f"{np.round(mean, decimals=3)} $\\pm$ {np.round(std, decimals=3)}" for mean, std in zip(means, stds)]
-        data = [f"${format_number(mean)} \\pm {format_number(std)}$" for mean, std in zip(means, stds)]
+        data = [f"{np.round(mean, decimals=3)} $\\pm$ {np.round(std, decimals=3)}" for mean, std in zip(means, stds)]
+        # data = [f"${format_number(mean)} \\pm {format_number(std)}$" for mean, std in zip(means, stds)]
         df[methods_name] = data
     df.insert(0, ' ', descriptions)
     # df.rename(index={0: 'Mean and Standard diviation of Model Gap after 5 steps'}, inplace=True)
@@ -180,8 +180,8 @@ def plot_fig(num_iters = 25,d_list = [10,1000,10000],folder_path = 'result/'):
         mse_list_start_avg = inner_dict.get('mse_list_start_avg')
         means = np.mean(mse_list_start_avg[:,5:], axis=1)
         stds = np.std(mse_list_start_avg[:,5:], axis=1)
-        # data = [f"{np.round(mean, decimals=3)} $\\pm$ {np.round(std, decimals=3)}" for mean, std in zip(means, stds)]
-        data = [f"${format_number(mean)} \\pm {format_number(std)}$" for mean, std in zip(means, stds)]
+        data = [f"{np.round(mean, decimals=3)} $\\pm$ {np.round(std, decimals=3)}" for mean, std in zip(means, stds)]
+        # data = [f"${format_number(mean)} \\pm {format_number(std)}$" for mean, std in zip(means, stds)]
         df[methods_name] = data
     df.insert(0, ' ', descriptions)
     # df.rename(index={0: 'Mean and Standard diviation of Accuracy after 5 steps'}, inplace=True)
