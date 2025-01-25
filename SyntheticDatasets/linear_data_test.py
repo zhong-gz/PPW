17a8465273cec51f250a471aa6a3d71646cef564
zhong-gz <510475503@qq.com>
2025-01-25 11:08:10 +0800

    refactor: update data transformation logic and adjust plot limits for better visualization

--- SyntheticDatasets/linear_data_test.py
+++ SyntheticDatasets/linear_data_test.py
@@ -8,25 +8,39 @@ def data_distribution_map(mu,X,y,model):
     # 将数组 a 归一化到 [0, 1]
     hat_y = model.predict(X)
     mean_hat_y = np.mean(hat_y)
-    scaler = MinMaxScaler()
-    y_transformed = scaler.fit_transform(y - mu*(hat_y-mean_hat_y)) + np.random.normal(0, 0.1, size=y.shape)
-    # y_transformed = y + mu*(hat_y-mean_hat_y) + np.random.normal(0, 0.1, size=y.shape)
-    
-    return y_transformed
+    # scaler = MinMaxScaler()
+    # y_transformed = scaler.fit_transform(y - mu*(hat_y-mean_hat_y)) + np.random.normal(0, 0.1, size=y.shape)
+    # y_transformed = y - mu*(hat_y-mean_hat_y) + np.random.normal(0, 0.1, size=y.shape)
+    # y_transformed = y - mu*(hat_y-0.5*mean_hat_y)
+    y_transformed = 1.67 * X + mu*hat_y
+    x_transformed = X #- mu*(hat_y-0.5*mean_hat_y)
+    return x_transformed,y_transformed
 
 # 生成线性回归数据集
 np.random.seed(0)  # 为了可重复性
 
-num_iters = 100
+num_iters = 50
 
-mu = 0.8
+# X = np.random.rand(200, 1) + 1.67  # 生成100个随机数作为自变量
+# # X_b = np.c_[np.ones((X.shape[0], 1)), X]
+# # scaler = MinMaxScaler()
+# y = 1.67 + 1.67 * X
+
+mu = 1
+
+# X = np.random.randn(200, 1) + 1.67
+# y =  1.67*X
+
+# model = Ridge(alpha = 0, fit_intercept=False) 
+# model.fit(X, y)
+
 
 for t in range(num_iters):
-    X = 2 * np.random.rand(200, 1)  # 生成100个随机数作为自变量
+    X = np.random.randn(200, 1) + 1.67   # 生成100个随机数作为自变量
     # X_b = np.c_[np.ones((X.shape[0], 1)), X]
-    scaler = MinMaxScaler()
-    y = 4 + 3 * X + 0.2*np.random.randn(200, 1)  # 生成因变量，添加一些噪声
-    y = scaler.fit_transform(y)
+    # scaler = MinMaxScaler()
+    y = 1.67 * X #+ 0.1*np.random.randn(200, 1)  # 生成因变量，添加一些噪声
+    # y = scaler.fit_transform(y)
     # percentage_to_shuffle = 0.2
     # num_elements_to_shuffle = int(len(y) * percentage_to_shuffle)
     # indices = np.random.choice(len(y), num_elements_to_shuffle, replace=False)
@@ -34,37 +48,38 @@ for t in range(num_iters):
     # y[indices] = elements_to_shuffle
 
     if t==0:
+        x_new = X
         y_new = y
         mse_start = 0
     else:
-        y_new = data_distribution_map(mu,X,y,model)
+        x_new,y_new = data_distribution_map(mu,X,y,model)
         hat_y = model.predict(X)
         mse_start = mean_squared_error(hat_y,y_new)
         # print('mse start:',mse_start)
 
     # 创建线性回归模型
-    model = Ridge(alpha = 0) #, fit_intercept=False) 
-    model.fit(X, y_new)  # 拟合模型
+    model = Ridge(alpha = 0, fit_intercept=False) 
+    model.fit(x_new, y_new)  # 拟合模型
 
-    hat_y = model.predict(X)
-    mse_end = mean_squared_error(hat_y,y_new)
+    # hat_y = model.predict(X)
+    # mse_end = mean_squared_error(hat_y,y_new)
     # print('mse end:',mse_end)
 
     # print(np.abs(mse_end-mse_start))
 
-    X_plot = np.array([[-2], [2]])  # 用于绘制预测线
+    X_plot = np.array([[-100], [100]])  # 用于绘制预测线
     # X_b_plot = np.c_[np.ones((X_plot.shape[0], 1)), X_plot]
     y_plot = model.predict(X_plot)
 
     plt.clf()
     # 绘制数据点和回归线
-    plt.scatter(X, y_new, color='blue', label='data')  # 绘制数据点
+    plt.scatter(x_new, y_new, color='blue', label='data')  # 绘制数据点
     plt.plot(X_plot, y_plot, color='red', label='regression function')  # 绘制回归线
     plt.xlabel('X')
     plt.ylabel('y')
     plt.title('ridge regression')
-    plt.xlim(-0.5, 2.5)  # 替换 x_min 和 x_max 为你想要的范围
-    plt.ylim(-0.1, 1.1)
+    plt.xlim(-4 ,4)  
+    plt.ylim(-8, 8)
     # plt.legend()
     plt.pause(0.2) 
 plt.show() 
--- test.py
+++ test.py
@@ -1,16 +1,7 @@
 import numpy as np
 
 def logistic_loss(y_true, y_pred):
-    """
-    计算逻辑回归的损失函数
-    
-    参数:
-    y_true: 真实标签，形状为 (n_samples,)
-    y_pred: 预测概率，形状为 (n_samples,)
-    
-    返回:
-    loss: 平均损失值
-    """
+
     # 确保输入是numpy数组
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
