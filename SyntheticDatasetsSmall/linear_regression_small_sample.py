3c30a3c5724763a60234e195739a195c98817b72
Zhong-GZ <510475503@qq.com>
2025-08-28 16:03:37 +0800

    Add PDF report for MSE analysis with d=8 and no standardization

--- SyntheticDatasetsSmall/linear_regression_small_sample.py
+++ SyntheticDatasetsSmall/linear_regression_small_sample.py
@@ -17,34 +17,32 @@ num_iters = 100
 d_list = [2,4,8,16] 
 num_experiments = 10
 map = 1
-# folder_path = 'SyntheticDatasets/result/'
-# folder_path = 'SyntheticDatasets/result_uniform/'
-# folder_path = 'SyntheticDatasetsSmall/result{n}/'
 
 np.random.seed(seed_value)
 random.seed(seed_value)
 n_features = 20
-folder_path = f'SyntheticDatasetsSmall/result/'
+folder_path = f'SyntheticDatasetsSmall/result_small/'
 os.makedirs(folder_path, exist_ok=True)
 strat_features = None
-ns = [0.05,0.1,0.2,1]
+# ns = [0.05,0.2,0.5,1]
+ns = [5,20,50,100]
 
-# for n_p in ns:
-#     print('n_p = ',n_p)
-#     n = int(n_p*100)
-#     X,y = linear_data_generation(n = n, n_features=n_features)
-#     d = X.shape[1]
-#     print('Sample number : ',n)
-#     print('Sample dimension : ',d)
-#     print('-'*50)
-#     # method 1
-#     model_gaps_avg,model_gaps_std,mse_list_start_avg,mse_list_start_std,mse_list_end_avg,mse_list_end_std,method_name = \
-#     method_1(X,y,num_iters,d_list,map = map,num_experiments = num_experiments,seed_value = seed_value,alpha_1 = 2.1)
-#     file_name_npy = f"{folder_path}{n_p}.npz"
-#     np.savez(file_name_npy, model_gaps_avg = model_gaps_avg, model_gaps_std = model_gaps_std,\
-#                 mse_list_start_avg = mse_list_start_avg, mse_list_start_std = mse_list_start_std,\
-#                 mse_list_end_avg = mse_list_end_avg, mse_list_end_std = mse_list_end_std)
-#     print(f"Data saved to {file_name_npy}")
+for n_p in ns:
+    print('n_p = ',n_p)
+    n = int(n_p)
+    X,y = linear_data_generation(n = n, n_features=n_features)
+    d = X.shape[1]
+    print('Sample number : ',n)
+    print('Sample dimension : ',d)
+    print('-'*50)
+    # method 1
+    model_gaps_avg,model_gaps_std,mse_list_start_avg,mse_list_start_std,mse_list_end_avg,mse_list_end_std,method_name = \
+    method_1(X,y,num_iters,d_list,map = map,num_experiments = num_experiments,seed_value = seed_value,alpha_1 = 2.1)
+    file_name_npy = f"{folder_path}{n_p}.npz"
+    np.savez(file_name_npy, model_gaps_avg = model_gaps_avg, model_gaps_std = model_gaps_std,\
+                mse_list_start_avg = mse_list_start_avg, mse_list_start_std = mse_list_start_std,\
+                mse_list_end_avg = mse_list_end_avg, mse_list_end_std = mse_list_end_std)
+    print(f"Data saved to {file_name_npy}")
 
 rmse_min = 0.2
 rmse_max = 5
